1ec914403625f3d26c7e7b64b9b6ae794d0768db
Michel Guilhen Gomes <michel.gomes@itixti.com.br>
2020-09-11 21:11:54 -0300

    models

---
+++ Stefanini.DotnetFilms/Models/Film.cs
@@ -0,0 +1,12 @@
+﻿namespace Stefanini.DotnetFilms.Models
+{
+    public class Film
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public string Synopsis { get; set; }
+        public int Release { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
---
+++ Stefanini.DotnetFilms/Models/Genre.cs
@@ -0,0 +1,8 @@
+﻿namespace Stefanini.DotnetFilms.Models
+{
+    public class Genre
+    {
+        public long Id { get; set; }
+        public string Description { get; set; }
+    }
+}
